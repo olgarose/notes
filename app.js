01e0419aed5d1741ae1b4ea98a9e7b452b7a995f
olgarose <olgarose@users.noreply.github.com>
2018-08-17 16:04:12 -0700

    Remove ability to create notes with same title

--- app.js
+++ app.js
@@ -12,7 +12,16 @@ console.log('Command: ', command);
 console.log('Yargs', argv);
 
 if (command === 'add') {
-    notes.addNote(argv.title, argv.body);
+    var note = notes.addNote(argv.title, argv.body);
+
+    if (note) {
+        console.log('Note created');
+        console.log('--');
+        console.log('Title: ${}', title);
+        console.log('Body: ${}', body);
+    } else {
+        console.log('Note title taken');
+    }
 } else if (command === 'list') {
     notes.getAll();
 } else if (command === 'read') {
